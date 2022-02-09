34a4d1154599c7a61931d1bcfd26ba508520dc1a
yalandr <52127093+yalandr@users.noreply.github.com>
2022-02-09 18:29:27 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 const scrollToForm = () => {
     document.querySelector('.form').scrollIntoView({behavior:"smooth"})
 }
+const scrollToAbout = () => {
+    document.querySelector('.section-about').scrollIntoView({behavior:"smooth"})
+}
+const scrollToBenefits = () => {
+    document.querySelector('.section-benefits').scrollIntoView({behavior:"smooth"})
+}
+const scrollToOffers = () => {
+    document.querySelector('.section-offers').scrollIntoView({behavior:"smooth"})
+}
+const scrollToSteps = () => {
+    document.querySelector('.section-steps').scrollIntoView({behavior:"smooth"})
+}
